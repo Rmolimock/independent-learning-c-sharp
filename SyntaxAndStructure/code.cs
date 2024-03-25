bc79c7c7471235d6d5ebd640a8927e1e0f18d405
Russell Molimock <russellmolimock@gmail.com>
2024-03-24 20:45:29 -0400

    Notes and first code example on syntax and structure of C#

---
+++ SyntaxAndStructure/code.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SyntaxAndStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
